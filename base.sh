f2687dcca49b324eefe9cda28e18dcd67be303cf
Gabriel Landais <g.landais@baya-solution.com>
2021-10-27 11:50:01 +0200

    Remove LFS

--- base.sh
+++ base.sh
@@ -7,9 +7,4 @@ currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID-$CUSTOM_ENV_CI_JOB_ID"
 
-if [ ! -f "${currentDir}/config.sh" ]; then
-    echo "Please create a config.sh"
-    exit 1
-fi
-
-source ${currentDir}/config.sh
+RESOURCE_GROUP="$CUSTOM_ENV_AZ_RESOURCE_GROUP"
--- setup-lfs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ${currentDir}/base.sh # Get variables from base.
-
-source setup-lfs-common.sh
